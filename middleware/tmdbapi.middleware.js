0254391925b0b8eda7a28a5a5015711508d209e1
Bryn McIvor <brynmcivor1@gmail.com>
2021-08-29 21:36:40 +1200

    convert promise to await in middleware api call

--- middleware/tmdbapi.middleware.js
+++ middleware/tmdbapi.middleware.js
@@ -8,37 +8,25 @@ const checkSeriesInDB = async (req, res, next) => {
     if (dbQuery) {
       // if present, assign entry data to res.locals.tmdb_data
       res.locals.tmdb_data = dbQuery;
-      console.log('TESTA', res.locals.tmdb_data);
       next();
     } else {
       // if not, call TMDB API and assign response to res.locals.tmdb_data
-      axios
-        .get(
-          `https://api.themoviedb.org/3/tv/${req.body.tmdb_id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`
-        )
-        .then(response => {
-          const data = { ...response.data };
-          const newSeriesData = {
-            title: data.name,
-            tmdb_id: data.id,
-            overview: data.overview,
-            first_air_date: data.first_air_date,
-            backdrop_path: data.backdrop_path,
-            poster_path: data.poster_path,
-            tagline: data.tagline,
-          };
-          const newDbEntry = TvSeries.create(newSeriesData);
-          return newDbEntry;
-        })
-        .then(newDbEntry => {
-          res.locals.tmdb_data = newDbEntry;
-        })
-        .then(() => {
-          next();
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      const apiCall = await axios.get(
+        `https://api.themoviedb.org/3/tv/${req.body.tmdb_id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`
+      );
+      const data = { ...apiCall.data };
+      const newSeriesData = {
+        title: data.name,
+        tmdb_id: data.id,
+        overview: data.overview,
+        first_air_date: data.first_air_date,
+        backdrop_path: data.backdrop_path,
+        poster_path: data.poster_path,
+        tagline: data.tagline,
+      };
+      const newDbEntry = await TvSeries.create(newSeriesData);
+      res.locals.tmdb_data = newDbEntry;
+      next();
     }
   } catch (err) {
     console.log(err);
