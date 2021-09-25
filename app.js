8af9d0b24f6af913320db309ee875fe079226650
Bryn McIvor <brynmcivor1@gmail.com>
2021-09-25 23:39:18 +1200

    change fallback port

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.use(watchlistRoutes);
 app.use(watchingRoutes);
 app.use(watchedRoutes);
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
